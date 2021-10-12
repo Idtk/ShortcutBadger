9a6a4bc5b0f95c73299b2c562205daff453e3634
Idtk <623680332@qq.com>
2021-10-12 11:24:06 +0800

    荣耀适配

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HihonorHomeBadger.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/HihonorHomeBadger.java
@@ -4,6 +4,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,17 +13,34 @@ import me.leolin.shortcutbadger.Badger;
 import me.leolin.shortcutbadger.ShortcutBadgeException;
 
 /**
- * @author Jason Ling
+ * @author Idtk
  */
 public class HihonorHomeBadger implements Badger {
 
     @Override
     public void executeBadge(Context context, ComponentName componentName, int badgeCount) throws ShortcutBadgeException {
-        Bundle localBundle = new Bundle();
-        localBundle.putString("package", context.getPackageName());
-        localBundle.putString("class", componentName.getClassName());
-        localBundle.putInt("badgenumber", badgeCount);
-        context.getContentResolver().call(Uri.parse( "content://com.hihonor.android.launcher.settings/badge/"), "change_badge", null, localBundle);
+        String URI_OLD = "content://com.huawei.android.launcher.settings/badge/";
+        String URI_NEW = "content://com.hihonor.android.launcher.settings/badge/";
+        Uri uri = Uri.parse(URI_NEW);
+        String type = context.getContentResolver().getType(uri);
+        if (TextUtils.isEmpty(type)) {
+            uri = Uri.parse(URI_OLD);
+            type = context.getContentResolver().getType(uri);
+            if (TextUtils.isEmpty(type)) {
+                uri = null;
+            }
+        }
+        try {
+            Bundle localBundle = new Bundle();
+            localBundle.putString("package", context.getPackageName());
+            localBundle.putString("class", componentName.getClassName());
+            localBundle.putInt("badgenumber", badgeCount);
+            if (uri != null) {
+                context.getContentResolver().call(uri, "change_badge", null, localBundle);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
